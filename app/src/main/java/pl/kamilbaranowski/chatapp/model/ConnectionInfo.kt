c7d2273e27c69c07e6d261a00598b03c70ab9ea9
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-05-08 18:47:14 +0200

    ConnectionInfo model class

---
+++ app/src/main/java/pl/kamilbaranowski/chatapp/model/ConnectionInfo.kt
@@ -0,0 +1,63 @@
+package pl.kamilbaranowski.chatapp.model
+
+import okhttp3.OkHttpClient
+import java.security.SecureRandom
+import java.security.cert.CertificateException
+import java.security.cert.X509Certificate
+import javax.net.ssl.HostnameVerifier
+import javax.net.ssl.SSLContext
+import javax.net.ssl.SSLSocketFactory
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
+class ConnectionInfo {
+    private val host = "https://192.168.1.21:8443/"
+
+
+    fun getHost(): String{
+        return this.host;
+    }
+
+    fun getUnsafeOkHttpClient(): OkHttpClient? {
+        return try {
+            // Create a trust manager that does not validate certificate chains
+            val trustAllCerts =
+                arrayOf<TrustManager>(
+                    object : X509TrustManager {
+                        @Throws(CertificateException::class)
+                        override fun checkClientTrusted(
+                            chain: Array<X509Certificate>,
+                            authType: String
+                        ) {
+                        }
+
+                        @Throws(CertificateException::class)
+                        override fun checkServerTrusted(
+                            chain: Array<X509Certificate>,
+                            authType: String
+                        ) {
+                        }
+
+                        override fun getAcceptedIssuers(): Array<X509Certificate> {
+                            return arrayOf()
+                        }
+                    }
+                )
+
+            // Install the all-trusting trust manager
+            val sslContext = SSLContext.getInstance("SSL")
+            sslContext.init(null, trustAllCerts, SecureRandom())
+            // Create an ssl socket factory with our all-trusting manager
+            val sslSocketFactory: SSLSocketFactory = sslContext.socketFactory
+            val builder = OkHttpClient.Builder()
+            builder.sslSocketFactory(
+                sslSocketFactory,
+                (trustAllCerts[0] as X509TrustManager)
+            )
+            builder.hostnameVerifier(HostnameVerifier { hostname, session -> true })
+            builder.build()
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
+    }
+}
