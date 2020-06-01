08ffc854f2d987949fcea3f0bf77b91bdcae241c
kamilbaranowski <kamil.baranowski93@gmail.com>
2020-06-01 09:08:22 +0200

    added default constructors

--- app/src/main/java/pl/kamilbaranowski/chatapp/model/Message.kt
+++ app/src/main/java/pl/kamilbaranowski/chatapp/model/Message.kt
@@ -2,4 +2,5 @@ package pl.kamilbaranowski.chatapp.model
 
 
 class Message(val sender: String, val receiver: String, val messageContent: String, val timestamp: Long) {
+    constructor(): this("", "", "", 0L)
 }
--- app/src/main/java/pl/kamilbaranowski/chatapp/model/User.kt
+++ app/src/main/java/pl/kamilbaranowski/chatapp/model/User.kt
@@ -5,5 +5,6 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-class User( val username: String, val password: String, val email: String, val status: String) : Parcelable {
+class User( val uid: String, val username: String, val password: String, val email: String, val status: String) : Parcelable {
+    constructor(): this("", "", "", "", "")
 }
